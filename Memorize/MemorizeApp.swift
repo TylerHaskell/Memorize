c207e94de576b5f3962aaefbe6a17a9864b609f8
Tyler Haskell <tyler.haskell@concur.com>
2021-11-15 19:14:29 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Haskell, Tyler on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
