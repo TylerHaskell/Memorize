5e4443e7fa5f9059ef9df6f990d40fea91a8efb9
Tyler Haskell <tyler.haskell@concur.com>
2021-12-02 10:25:39 -0600

    Complete Assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+
+    @State var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    var vehicleEmojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    var sportEmojis = ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🥏", "🎱", "🏒", "⛳️", "🎣", "🥌", "🏂", "🏊🏼‍♂️", "🏹", "🥊", "⛷", "⛸"]
+    var animalEmojis = ["🦆", "🦅", "🦉", "🐣", "🐺", "🐍", "🐢", "🐙", "🦑", "🦈", "🐊", "🐅", "🐘", "🐓", "🦌", "🐕", "🦃", "🐈‍⬛", "🦚", "🦩", "🐿"]
+    
     @State var emojiCount = 6
+    
     var body: some View {
         VStack {
+            HStack {
+                Text("Memorize!")
+                    .font(.title)
+                    .foregroundColor(Color.black)
+            }
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))] ) {
                     ForEach(emojis[0..<emojiCount], id: \.self) { emojis in
@@ -22,34 +33,57 @@ struct ContentView: View {
             .foregroundColor(.red)
             Spacer()
             HStack {
-                remove
                 Spacer()
-                add
+                vehicles
+                Spacer()
+                sports
+                Spacer()
+                animals
+                Spacer()
             }
-            .font(.largeTitle)
-            .padding(.horizontal)
         }
         .padding(.horizontal)
     }
     
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
+    var vehicles: some View {
+        Button(action: {
+            emojis = vehicleEmojis.shuffled()
+            emojiCount = Int.random(in: 4..<vehicleEmojis.count)
+        }) {
+            VStack {
+                Image(systemName: "car")
+                    .font(.largeTitle)
+                Text("Vehicles")
             }
-        } label: {
-            Image(systemName: "minus.circle")
         }
     }
-    var add: some View {
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
+    
+    var sports: some View {
+        Button(action: {
+            emojis = sportEmojis.shuffled()
+            emojiCount = Int.random(in: 4..<sportEmojis.count)
+        }) {
+            VStack {
+                Image(systemName: "sportscourt")
+                    .font(.largeTitle)
+                Text("Sports")
             }
-        } label: {
-            Image(systemName: "plus.circle")
         }
     }
+    
+    var animals: some View {
+        Button(action: {
+            emojis = animalEmojis.shuffled()
+            emojiCount = Int.random(in: 4..<animalEmojis.count)
+        }) {
+            VStack {
+                Image(systemName: "pawprint")
+                    .font(.largeTitle)
+                Text("Animals")
+            }
+        }
+    }
+    
 }
 
 struct CardView: View {
@@ -77,14 +111,10 @@ struct CardView: View {
 
 
 
-
-
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
+            .preferredColorScheme(.none)
         
     }
 }
